888b7a57c5b36a4dd7b1bf975a03717d7248f496
mohitarora249 <mohitarora249@gmail.com>
2024-02-28 00:07:18 +0530

    logout handler updated

--- src/app/(root)/_components/user-nav.tsx
+++ src/app/(root)/_components/user-nav.tsx
@@ -4,19 +4,36 @@ import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "~/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 
 const UserNav = () => {
   const { data } = useSession();
-  const userName = data?.user.name?.split(" ").map(s => s.charAt(0)).join("");
+  const userName = data?.user.name
+    ?.split(" ")
+    .map((s) => s.charAt(0))
+    .join("");
   const redirectToProfilePageHandler = () => redirect("/profile");
-
+  const logoutHandler = () => {
+    signOut().then(() => redirect("/"));
+  };
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src={data?.user.image ?? ""} alt={data?.user.name ?? ""} />
+            <AvatarImage
+              src={data?.user.image ?? ""}
+              alt={data?.user.name ?? ""}
+            />
             <AvatarFallback>{userName}</AvatarFallback>
           </Avatar>
         </Button>
@@ -24,7 +41,9 @@ const UserNav = () => {
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{data?.user.name}</p>
+            <p className="text-sm font-medium leading-none">
+              {data?.user.name}
+            </p>
             <p className="text-xs leading-none text-muted-foreground">
               {data?.user.email}
             </p>
@@ -38,13 +57,13 @@ const UserNav = () => {
           </DropdownMenuItem>
         </DropdownMenuGroup> */}
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => signOut()}>
+        <DropdownMenuItem onClick={logoutHandler}>
           Log out
           <DropdownMenuShortcut>⌘L</DropdownMenuShortcut>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}
+  );
+};
 
 export default UserNav;
