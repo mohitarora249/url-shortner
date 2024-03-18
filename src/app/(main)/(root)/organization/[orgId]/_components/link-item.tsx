65520b7acfabadd934ce4edb4b7f087a9414b338
mohitarora249 <mohitarora249@gmail.com>
2024-03-18 15:10:49 +0530

    Update link-item.tsx

--- src/app/(main)/(root)/organization/[orgId]/_components/link-item.tsx
+++ src/app/(main)/(root)/organization/[orgId]/_components/link-item.tsx
@@ -59,7 +59,7 @@ const LinkItem = ({ shortLink, id, link, linkType, expirationTime }: Props) => {
   const deleteLinkHandler = () => deleteLinkById({ id });
   const onBanLinkClickHandler = () => expireLinkById({ id });
   const markLinkActiveFromDeletedById = () => activateLinkById({ id });
-  const copyLinkHandler = () => copyToClipboard(link);
+  const copyLinkHandler = () => copyToClipboard(shortLink);
   const onLinkMouseOverHandler = async () => {
     const linkQR = await generateQRCode(url);
     setQrCode(linkQR);
